39eef7839adbe3c3730221ea5642366932cad059
Paridhi03 <98459098+Paridhi03@users.noreply.github.com>
2023-05-05 11:00:36 +0530

    Day-4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app1/Screens/Home_Screen.dart';
 import 'package:flutter_app1/Screens/Login_Screen.dart';
 
+import 'package:google_fonts/google_fonts.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -12,7 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       //  home: HomeScreen(),
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       routes: {
         "/": ((context) => LoginScreen()),
